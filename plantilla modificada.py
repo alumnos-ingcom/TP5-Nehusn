bb952d4fc4b230bbc72d1d03e6e995f4a84dfc40
Red Crossbones <gastonnehuen@outlook.com>
2021-06-10 14:26:13 -0300

    Create plantilla modificada.py

---
+++ plantilla modificada.py
@@ -0,0 +1,12 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo=" "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+
+    
+if __name__ == "__main__":
+    prueba()
