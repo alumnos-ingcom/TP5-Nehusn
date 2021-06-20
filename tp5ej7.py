527b3ed23d4db7022c195142772d94afe0f4da78
Red Crossbones <gastonnehuen@outlook.com>
2021-06-20 18:43:39 -0300

    Create tp5ej7.py

---
+++ tp5ej7.py
@@ -0,0 +1,24 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehus
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+from saludo import saludo
+def distancia(valorA, valorB):
+    distanciaAB=valorB-valorA
+    if distanciaAB<0:
+        distanciaAB=distanciaAB*-1
+    else:
+        pass
+    return distanciaAB
+
+def prueba():
+    mensaje="Distancias"
+    saludo(mensaje)
+    valorA=float(input("Ingrese valor A: "))
+    valorB=float(input("Ingrese valor B: "))
+    distancia(valorA,valorB)
+    print(f"La distancia entre {valorA} y {valorB} es:  {distancia(valorA, valorB)}")
+
+if __name__ == "__main__":
+    prueba()
+
