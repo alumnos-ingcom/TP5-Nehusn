d7e3e9ec4e078e184ef405250b47ce4dfc3d5ac2
Red Crossbones <gastonnehuen@outlook.com>
2021-06-20 17:32:53 -0300

    Archivo necesario para import

--- saludo.py
+++ saludo.py
@@ -2,11 +2,8 @@
 # Gastón Nehuen Rodriguez Valdez - @nehusn
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-def prueba():
-    saludo=" "
-    saludo_titulo= saludo.upper()
+def saludo(mensaje):
+    titulo=mensaje
+    saludo_titulo= titulo.upper()
     print(saludo_titulo+"\n")
-
     
-if __name__ == "__main__":
-    prueba()
