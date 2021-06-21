a40c23aa1aa451e8c7e23957e66a177403a8bdba
Red Crossbones <gastonnehuen@outlook.com>
2021-06-21 19:28:40 -0300

    Creada funcion de retorno binario y cambio de obtencion de numeros

--- tp5ej10.py
+++ tp5ej10.py
@@ -1,5 +1,5 @@
 ################
-# Gastón Nehuen Rodriguez Valdez - @nehus
+# Gastón Nehuen Rodriguez Valdez - @nehusn
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 from saludo import saludo
@@ -7,14 +7,24 @@ def a_binario(num):
     num_binario=format(num,"b")
     return num_binario
 
+def a_numero(binario):
+    num_normal=int(binario,2)
+    return num_normal
+
 def prueba():
     mensaje="Texto binario"
     saludo(mensaje)
     num=int(input("Ingrese número: "))
+    binario=input("Ingrese binario: ")
     a_binario(num)
+    a_numero(binario)
     resultado=a_binario(num)
-    print(f"El numero ingresado es: {num}")
+    resultado_1=a_numero(binario)
+    
+    print(f"\nEl numero ingresado es: {num}")
     print(f"El numero en binario es: {resultado}")
+    print(f"\nEl binario ingresado es: {binario}")
+    print(f"El numero convertido es: {resultado_1}")
     
 if __name__ == "__main__":
     prueba()
