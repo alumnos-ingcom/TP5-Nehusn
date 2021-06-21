ade7a93a6e077306c4b202d4d0d07654811c00d3
Red Crossbones <gastonnehuen@outlook.com>
2021-06-21 19:15:54 -0300

    Create tp5ej10.py

---
+++ tp5ej10.py
@@ -0,0 +1,21 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehus
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+from saludo import saludo
+def a_binario(num):
+    num_binario=bin(num)
+    return num_binario
+
+def prueba():
+    mensaje="Texto binario"
+    saludo(mensaje)
+    num=int(input("Ingrese número: "))
+    a_binario(num)
+    resultado=a_binario(num)
+    print(f"El numero ingresado es: {num}")
+    print(f"El numero en binario es: {resultado}")
+    
+if __name__ == "__main__":
+    prueba()
+
