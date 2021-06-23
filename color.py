ea7ba1aa1da6beceb930b9f6fe8f1fd62d0d230f
Red Crossbones <gastonnehuen@outlook.com>
2021-06-22 22:40:21 -0300

    Update color.py

--- color.py
+++ color.py
@@ -1,26 +1,23 @@
 import random
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def color_letra():
     num = random.randint(31,36)
-    color=f"\x1b[0;{num}m"
+    color=f"\x1b[{num};{num}m"
     return color
+
 def color_fondo():
-    num = random.randint(41,46)
-    color=f"\x1b[0;{num}m"
+    num = random.randint(40,47)
+    color=f"\x1b[{num};{num}m"
     return color
-    
+
+def color_rgb():
+    letra = random.randint(31,36)
+    fondo = random.randint(40,47)
+    num=random.randint(0,7)
+    color=f"\x1b[{num};{letra};{fondo}m"
+    return color
 def prueba():
     
     for i in range(0,100):
-        color_a=color_letra()
+        color_a=color_rgb()
         print(f"{color_a}",i)
